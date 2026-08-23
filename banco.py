197e49290a7662f9a65eeb3abe60e53282a1fb1a
JoaoP-Mendes <asstecmendes@gmal.com>
2026-08-23 12:01:59 -0400

    Adicao classe banco para conectar, desconectar e executar

---
+++ banco.py
@@ -0,0 +1,39 @@
+import pymysql.connections
+from config import DB_CONFIG
+
+class BancoDados():
+    def __init__(self):
+        self.conexao = None
+
+    def conectar(self):
+        try:
+            self.conexao = pymysql.connections.Connection(**DB_CONFIG)
+
+        except Exception as e:
+            print(f"An error occured: {e}")
+
+    def desconectar(self):
+        try:
+            self.conexao.close()
+
+        except Exception as e:
+            print(f"An error occured: {e}")
+
+    def executar(self, query, valor = ()):
+        try:
+            cursor = self.conexao.cursor()
+            cursor.execute(query, valor)
+
+            if query.strip().upper().startswith("SELECT"):
+                resultado = cursor.fetchall()
+                return resultado
+            else:
+                self.conexao.commit()
+                return cursor.lastrowid
+
+        except Exception as e:
+            print(f"An error occured: {e}")
+
+
+"""query = "SELECT * FROM livros WHERE id = %s"
+self.conexao.executar(query, (id,))"""
